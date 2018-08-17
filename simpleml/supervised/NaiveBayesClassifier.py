021ae29770cb0cbbd5c1d5e7f5b9d2b05870063c
wesley Kaizer <kaizerwesley@gmail.com>
2018-08-17 19:55:50 -0300

    Corrected NaiveBayesClassifier

--- simpleml/supervised/NaiveBayesClassifier.py
+++ simpleml/supervised/NaiveBayesClassifier.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 class NaiveBayesClassifier:
 
-    def __init__(self):
-        pass
+    def __init__(self, alpha=1):
+        self.alpha = alpha # Laplace smoothing parameter
 
     def fit(self, X, y):
         self.X = X
@@ -19,48 +19,52 @@ class NaiveBayesClassifier:
         # Target classes
         classes = np.unique(y)
 
-        print(classes)
+        # predicted classes
+        y_pred = np.zeros((x.shape[0], 1))
 
-        # A priori probabilities
+        # A priori probability
         priori_probs = {}
         for classe in classes:
             priori_probs[classe] = sum(y==classe) / y.shape[0]
 
-        prods = {}
-        for classe in classes:
-            prod = 1.0
+        for i in range(x.shape[0]):
+            posteriori_probs = {}
+            for classe in classes:
+                posteriori_probs[classe] = 1.0
 
-            for feature in range(x.shape[1]):
-                samples = np.where(X[:, feature]==x[0, feature])
-                samples = sum(y[samples]==classe)
-                prod *= samples / priori_probs[classe]
+                for feature in range(x.shape[1]):
+                    idx = np.where(X[:, feature]==x[i, feature])
+                    samples = sum(y[idx]==classe)
+                    # Conditional probability
+                    prob = (samples + self.alpha) / (sum(y==classe) + self.alpha * len(classes))
+                    posteriori_probs[classe] *= prob
 
-            prods[classe] = priori_probs[classe] * prod
+                # A posteriori probability
+                posteriori_probs[classe] *= priori_probs[classe]
 
-        m = -np.inf
-        y_pred = -1
-        for classe in classes:
-            p = prods[classe]
-            if p > m:
-                m = p
-                y_pred = classe
+            # Predict class with the greatest a posteriori probability
+            max_prob = -np.inf
+            for classe in classes:
+                prob = posteriori_probs[classe]
+                if prob > max_prob:
+                    max_prob = prob
+                    y_pred[i, 0] = classe
+        
         return y_pred
 
 
 if __name__ == '__main__':
-    # TODO: Test deeply NaiveBayesClassifier
-
     X, y = make_classification(n_samples=500, n_features=10, n_informative=10, 
-                               n_redundant=0, n_repeated=0, n_classes=2)
+                               n_redundant=0, n_repeated=0, n_classes=5)
 
-    # By now only works with categorical features
+    # By now, only works with categorical features
     # TODO: Generalize to numeric and mixed features
     X = abs(X.astype(int))
 
     model = NaiveBayesClassifier()
     model.fit(X, y)
 
-    x = X[0, :].reshape((1, -1))
     y = y.reshape((-1, 1))
-
-    print('Accuracy:', model.predict(x)==y[0])
+        
+    y_pred = model.predict(X)
+    print('Accuracy:', (sum(y_pred==y) / y.shape[0]))
