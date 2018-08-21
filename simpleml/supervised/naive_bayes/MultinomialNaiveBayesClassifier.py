da4a2107972e85f612cd5c7127a1555a8fe9ba7c
kaizerw <kaizerwesley@gmail.com>
2018-08-21 15:30:51 -0300

    Added MultinomialNaiveBayesClassifier

---
+++ simpleml/supervised/naive_bayes/MultinomialNaiveBayesClassifier.py
@@ -0,0 +1,65 @@
+import numpy as np
+from sklearn.datasets import make_classification
+import matplotlib.pyplot as plt
+
+
+class MultinomialNaiveBayesClassifier:
+
+    def __init__(self, alpha=1):
+        self.alpha = alpha # Laplace smoothing parameter
+        
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        self.n_samples, self.n_features = self.X.shape
+        self.classes = np.unique(self.y)
+        self.n_classes = len(self.classes)
+
+        # A priori probabilities
+        self.priori_probs = {}
+        for classe in self.classes:
+            n_samples_classe = sum(y == classe)
+            self.priori_probs[classe] = n_samples_classe / self.n_samples
+
+        # Precompute conditional probabilities
+        self.conditional_probs = {}
+        for classe in self.classes:
+            n_total = sum(sum(self.X[self.y == classe, :]))
+            for feature in range(self.n_features):
+                n_samples = sum(self.X[self.y == classe, feature])
+                prob = (n_samples + self.alpha) / (n_total + self.alpha * self.n_features)
+                self.conditional_probs[classe, feature] = prob
+
+    def predict(self, X):
+        n_samples_test = X.shape[0]
+        y_pred = np.zeros(n_samples_test)
+
+        for i in range(n_samples_test):
+            posteriori_probs = np.ones(self.n_classes)
+            for classe in self.classes:
+                for feature in range(self.n_features):
+                    if X[i, feature] > 0:
+                        prob = self.conditional_probs[classe, feature]
+                        posteriori_probs[classe] *= prob
+
+                # A posteriori probability
+                posteriori_probs[classe] *= self.priori_probs[classe]
+            
+            # Predict the class with greatest a posteriori probability
+            y_pred[i] = np.argmax(posteriori_probs)
+        
+        return y_pred
+    
+
+if __name__ == '__main__':
+    X, y = make_classification(n_samples=500, n_features=100, n_informative=100, 
+                               n_redundant=0, n_repeated=0, n_classes=5)
+    
+    X = abs(X.astype(int))
+
+    model = MultinomialNaiveBayesClassifier()
+    model.fit(X, y)
+
+    n_samples_test = X.shape[0]
+    y_pred = model.predict(X)
+    print('Accuracy:', (sum(y_pred == y) / n_samples_test))
