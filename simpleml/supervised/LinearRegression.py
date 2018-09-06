898da0639f1af880c6dbf23e53e9e1d1d2256afa
wesley Kaizer <kaizerwesley@gmail.com>
2018-09-05 21:17:09 -0300

    Refactored LinearRegression and LogisticRegression

--- simpleml/supervised/LinearRegression.py
+++ simpleml/supervised/LinearRegression.py
@@ -5,7 +5,7 @@ from scipy.optimize import minimize
 class LinearRegression:
 
     def __init__(self, alpha=1e-3, max_iter=1e4, tol=1e-4, lambd=0, 
-                 method='batch_gradient_descent'):
+                 method='batch__gradient_descent'):
         self.alpha = alpha # Learning rate
         self.max_iter = int(max_iter) # Max iterations
         self.tol = tol # Error tolerance
@@ -17,16 +17,17 @@ class LinearRegression:
         self.w = np.zeros(n)
         self.b = 0
 
-        if self.method == 'batch_gradient_descent':
+        if self.method == 'batch__gradient_descent':
             for _ in range(self.max_iter):
                 params = np.concatenate((self.w, [self.b]))
 
-                d = self._gradient(params, X, y, self.lambd)
+                d = self.__gradient(params, X, y, self.lambd)
                 dw, db = d[:-1], d[-1]
+                
                 self.w -= self.alpha * dw
                 self.b -= self.alpha * db
 
-                cost = self._cost(params, X, y, self.lambd)
+                cost = self.__cost(params, X, y, self.lambd)
 
                 if cost <= self.tol:
                     break
@@ -35,34 +36,34 @@ class LinearRegression:
             options = {'gtol': self.tol, 'maxiter': self.max_iter}
             args = (X, y, self.lambd)
             params = np.concatenate((self.w, [self.b]))
-            res = minimize(self._cost, params, 
-                           jac=self._gradient, args=args, 
+            res = minimize(self.__cost, params, 
+                           jac=self.__gradient, args=args, 
                            method=self.method, options=options)
             self.w, self.b = res.x[:-1], res.x[-1]
 
         return self
 
-    def _activation(self, X, w, b):
-        return (X @ w) + b
+    def __activation(self, X, w, b):
+        return X @ w + b
 
     def predict(self, X):
-        return self._activation(X, self.w, self.b)
+        return self.__activation(X, self.w, self.b)
 
-    def _gradient(self, params, X, y_true, lambd):
+    def __gradient(self, params, X, y, lambd):
         w, b = params[:-1], params[-1]
         m = X.shape[0]
-        y_pred = self._activation(X, w, b)
-        error = y_pred - y_true
-        dw = (1 / m) * sum((error * X.T).T)
-        db = (1 / m) * sum(error)
+        z = self.__activation(X, w, b)
+        dz = z - y
+        dw = (1 / m) * sum((dz * X.T).T)
+        db = (1 / m) * sum(dz)
         dw += ((lambd / m) * sum(w))
         return np.concatenate((dw, [db]))
 
-    def _cost(self, params, X, y_true, lambd):
+    def __cost(self, params, X, y, lambd):
         w, b = params[:-1], params[-1]
         m = X.shape[0]
-        y_pred = self._activation(X, w, b)
-        error = y_pred - y_true
-        cost = (1 / (2 * m)) * sum(error ** 2)
+        z = self.__activation(X, w, b)
+        dz = z - y
+        cost = (1 / (2 * m)) * sum(dz ** 2)
         cost += ((lambd / (2 * m)) * sum(w))
         return cost
--- simpleml/supervised/LogisticRegression.py
+++ simpleml/supervised/LogisticRegression.py
@@ -5,7 +5,7 @@ from scipy.optimize import minimize
 class LogisticRegression:
 
     def __init__(self, alpha=1e-3, max_iter=1e4, tol=1e-4, lambd=0, 
-                 threshold=0.5, method='batch_gradient_descent'):
+                 threshold=0.5, method='batch__gradient_descent'):
         self.alpha = alpha # Learning rate
         self.max_iter = int(max_iter) # Max iterations
         self.tol = tol # Error tolerance
@@ -17,65 +17,68 @@ class LogisticRegression:
         if len(np.unique(y)) > 2:
             raise Exception('This classifier only works with two classes')
 
-        n_samples, n_features = X.shape
-        X = np.hstack((np.ones((n_samples, 1)), X))
+        n = X.shape[1]
         
-        self.theta = np.zeros(n_features + 1)
-        self.costs = []
+        self.w = np.zeros(n)
+        self.b = 0
 
-        if self.method == 'batch_gradient_descent':
+        if self.method == 'batch__gradient_descent':
             for _ in range(self.max_iter):
-                grad = self._gradient(self.theta, X, y, self.lambd)
-                self.theta -= self.alpha * grad
+                params = np.concatenate((self.w, [self.b]))
 
-                cost = self._cost(self.theta, X, y, self.lambd)
-                self.costs.append(cost)
+                d = self.__gradient(params, X, y, self.lambd)
+                dw, db = d[:-1], d[-1]
 
+                self.w -= self.alpha * dw
+                self.b -= self.alpha * db
+
+                cost = self.__cost(params, X, y, self.lambd)
+                
                 if cost <= self.tol:
                     break
                     
         else:
             options = {'gtol': self.tol, 'maxiter': self.max_iter}
             args = (X, y, self.lambd)
-            res = minimize(self._cost, self.theta, 
-                           jac=self._gradient, args=args, 
+            params = np.concatenate((self.w, [self.b]))
+            res = minimize(self.__cost, params, 
+                           jac=self.__gradient, args=args, 
                            method=self.method, options=options)
-            self.theta = res.x
+            self.w, self.b = res.x[:-1], res.x[-1]
 
         return self
 
     def _sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
 
-    def _activation(self, X, theta):
-        n_samples_test = X.shape[0]
-        if X.shape[1] != theta.shape[0]:
-            X = np.hstack((np.ones((n_samples_test, 1)), X))
-        return self._sigmoid(theta @ X.T)
+    def __activation(self, X, w, b):
+        return self._sigmoid(X @ w + b)
 
     def predict(self, X):
-        return np.where(self._activation(X, self.theta) >= self.threshold, 1, 0)
+        activation = self.__activation(X, self.w, self.b)
+        return np.where(activation >= self.threshold, 1, 0)
 
     def predict_proba(self, X):
-        pos = np.reshape(self._activation(X, self.theta), (-1, 1))
+        activation = self.__activation(X, self.w, self.b)
+        pos = np.reshape(activation, (-1, 1))
         neg = 1 - pos
         return np.hstack((neg, pos))
 
-    def _gradient(self, theta, X, y_true, lambd):
-        n_samples = X.shape[0]
-        y_pred = self._activation(X, theta)
-        error = y_pred - y_true
-        grad = (1 / n_samples) * sum((error * X.T).T)
-        grad[1:] += (lambd / n_samples) * theta[1:]
-        return grad
+    def __gradient(self, params, X, y_true, lambd):
+        w, b = params[:-1], params[-1]
+        m = X.shape[0]
+        z = self.__activation(X, w, b)
+        dz = z - y_true
+        dw = (1 / m) * sum((dz * X.T).T)
+        db = (1 / m) * sum(dz)
+        dw += (lambd / m) * w
+        return np.concatenate((dw, [db]))
 
-    def _cost(self, theta, X, y_true, lambd):
-        n_samples = X.shape[0]
-        y_pred = self._activation(X, theta)
-        left = -y_true * np.log(y_pred)
-        right = -(1 - y_true) * np.log(1 - y_pred)
-        left[np.isnan(left)] = -np.inf
-        right[np.isnan(right)] = -np.inf
-        cost = (1 / n_samples) * sum(left + right)
-        cost += (lambd / (2 * n_samples)) * sum(theta[1:] ** 2)
+    def __cost(self, params, X, y_true, lambd):
+        w, b = params[:-1], params[-1]
+        m = X.shape[0]
+        z = self.__activation(X, w, b)
+        dz = -y_true * np.log(z) - (1 - y_true) * np.log(1 - z)
+        cost = (1 / m) * sum(dz)
+        cost += (lambd / (2 * m)) * sum(w ** 2)
         return cost
