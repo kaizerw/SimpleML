4d44bfec3a7c7f91dbe8778b1d75202abf3d3ffa
wesley Kaizer <kaizerwesley@gmail.com>
2018-09-29 16:49:00 -0300

    Check shallow neural network

--- tests.py
+++ tests.py
@@ -551,9 +551,9 @@ if __name__ == '__main__':
     tests = [
              #test_linear_regression, 
              #test_logistic_regression,
-             test_support_vector_machine_classifier, 
-             test_support_vector_machine_regressor, 
-             #test_shallow_neural_network, 
+             #test_support_vector_machine_classifier, 
+             #test_support_vector_machine_regressor, 
+             test_shallow_neural_network, 
              #test_deep_neural_network,  
              #test_KNN_classifier, 
              #test_KNN_regressor,
